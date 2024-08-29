4740ff1eba2f67aad0a34a8b34c2b40edb007733
vannam25032005 <25.nguyenvannam.11a1@gmail.com>
2024-08-29 23:59:10 +0700

    Delete SecondPrj directory

--- SecondPrj/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"iuh.fit.se"}];updateSearchResults();
--- SecondPrj/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"iuh.fit.se","l":"Rectangle"},{"p":"iuh.fit.se","l":"TestRectangle"}];updateSearchResults();
